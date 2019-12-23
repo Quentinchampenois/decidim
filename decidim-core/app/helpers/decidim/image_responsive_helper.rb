341450ee17d4c882636ba1ca654e684aa3b5f048
quentinchampenois <quentin.champenois@eemi.com>
2019-12-23 17:59:47 +0100

    Fix rubocop offenses

--- decidim-core/app/helpers/decidim/image_responsive_helper.rb
+++ decidim-core/app/helpers/decidim/image_responsive_helper.rb
@@ -3,7 +3,6 @@
 module Decidim
   # A Helper to render image according to the device screen
   module ImageResponsiveHelper
-
     def image_responsive(image, options = {})
       # https://developer.mozilla.org/en-US/docs/Web/HTML/Element/picture
       # https://gist.github.com/armandfardeau/0730c917e4a2c82871b85d690b1b169f
@@ -16,6 +15,5 @@ module Decidim
         concat image_tag(image.url, options)
       end
     end
-
   end
 end
--- decidim-core/spec/helpers/decidim/image_responsive_helper_spec.rb
+++ decidim-core/spec/helpers/decidim/image_responsive_helper_spec.rb
@@ -5,28 +5,19 @@ require "spec_helper"
 module Decidim
   include CarrierWave::Test::Matchers
   describe ImageResponsiveHelper, type: :helper do
-
-    # Si l'image n'existe pas
-    # Si l'image n'est pas une image
-
     describe "#image_responsive" do
-      # let!(:banner_image) { Decidim::Dev.test_file("city2.jpeg", "image/jpeg") }
-      # let!(:participatory_process) { create(:participatory_process, banner_image: banner_image) }
       let(:participatory_process) { create(:participatory_process) }
 
-      # OK
       it "includes picture html tag " do
         expect(helper.image_responsive(participatory_process.banner_image)).to include("<picture>")
         expect(helper.image_responsive(participatory_process.banner_image)).to include("<source media=\"(max-width: 768px)\" srcset=\"/uploads/decidim/participatory_process/banner_image/#{participatory_process.id}/xs_city2.jpeg\"></source>")
         expect(helper.image_responsive(participatory_process.banner_image)).to include("<source media=\"(max-width: 992px)\" srcset=\"/uploads/decidim/participatory_process/banner_image/#{participatory_process.id}/md_city2.jpeg\"></source>")
       end
 
-      # OK
       it "displays a fallback img tag" do
         expect(helper.image_responsive(participatory_process.banner_image)).to include("<img src=\"/uploads/decidim/participatory_process/banner_image/#{participatory_process.id}/city2.jpeg\" />")
       end
 
-      # OK
       context "when there is a missing version" do
         before do
           BannerImageUploader.versions.delete(:xs)
@@ -38,7 +29,6 @@ module Decidim
           expect(helper.image_responsive(participatory_process.banner_image)).not_to include("<source media=\"(max-width: 768px)\" srcset=\"/uploads/decidim/participatory_process/banner_image/#{participatory_process.id}/xs_city2.jpeg\"></source>")
         end
       end
-
     end
   end
 end
