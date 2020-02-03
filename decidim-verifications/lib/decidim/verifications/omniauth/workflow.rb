5d65b7b66d4bd68cbff6d60cdbed06eb1f20041d
moustachu <git@moustachu.net>
2020-02-03 23:34:15 +0100

    remove default workflow

--- decidim-verifications/lib/decidim/verifications/omniauth/workflow.rb
+++ decidim-verifications/lib/decidim/verifications/omniauth/workflow.rb
@@ -1,34 +1,9 @@
 # frozen_string_literal: true
 
-Decidim::Verifications.register_workflow(:decidim) do |workflow|
-  workflow.engine = Decidim::Verifications::Omniauth::Engine
-  workflow.admin_engine = Decidim::Verifications::Omniauth::AdminEngine
-  # workflow.form = "Decidim::Verifications::Omniauth::OmniauthAuthorizationForm"
-  workflow.omniauth_provider = :decidim
-  workflow.expires_in = 1.month
-end
-
-Decidim::Verifications.register_workflow(:france_connect_uid) do |workflow|
-  workflow.engine = Decidim::Verifications::Omniauth::Engine
-  workflow.admin_engine = Decidim::Verifications::Omniauth::AdminEngine
-  # workflow.form = "Decidim::Verifications::Omniauth::OmniauthAuthorizationForm"
-  workflow.omniauth_provider = :france_connect_uid
-  workflow.anti_affinity = [:france_connect_profile]
-end
-
-Decidim::Verifications.register_workflow(:france_connect_profile) do |workflow|
-  workflow.engine = Decidim::Verifications::Omniauth::Engine
-  workflow.admin_engine = Decidim::Verifications::Omniauth::AdminEngine
-  # workflow.action_authorizer = "Decidim::Verifications::Omniauth::ActionAuthorizer"
-  # workflow.form = "Decidim::Verifications::Omniauth::OmniauthAuthorizationForm"
-  workflow.omniauth_provider = :france_connect_profile
-  workflow.anti_affinity = [:france_connect_uid]
-  workflow.minimum_age = 18
-end
-
-Decidim::Verifications.register_workflow(:saml) do |workflow|
-  workflow.engine = Decidim::Verifications::Omniauth::Engine
-  workflow.admin_engine = Decidim::Verifications::Omniauth::AdminEngine
-  # workflow.form = "Decidim::Verifications::Omniauth::OmniauthAuthorizationForm"
-  workflow.omniauth_provider = :saml
-end
+# Decidim::Verifications.register_workflow(:decidim) do |workflow|
+#   workflow.engine = Decidim::Verifications::Omniauth::Engine
+#   workflow.admin_engine = Decidim::Verifications::Omniauth::AdminEngine
+#   # workflow.form = "Decidim::Verifications::Omniauth::OmniauthAuthorizationForm"
+#   workflow.omniauth_provider = :decidim
+#   workflow.expires_in = 1.month
+# end
