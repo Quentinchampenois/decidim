ba339f9413179c91cd0b12dd5bfd4bb6a1e35237
quentinchampenois <quentin.champenois7@gmail.com>
2020-03-31 18:36:06 +0200

    change cta redirection to initiatives_path

--- decidim-initiatives/app/controllers/decidim/initiatives/admin/initiatives_controller.rb
+++ decidim-initiatives/app/controllers/decidim/initiatives/admin/initiatives_controller.rb
@@ -107,7 +107,7 @@ module Decidim
 
           SendInitiativeToTechnicalValidation.call(current_initiative, current_user) do
             on(:ok) do
-              redirect_to edit_initiative_path(current_initiative), flash: {
+              redirect_to Decidim::Initiatives::Engine.routes.url_helpers.initiatives_path, flash: {
                 notice: I18n.t(
                   "success",
                   scope: %w(decidim initiatives admin initiatives edit)
