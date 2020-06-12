be2b52ee68d8faf0c18a41839bb24e658b5896b0
Armand Fardeau <fardeauarmand@gmail.com>
2020-06-12 16:56:50 +0200

    Add cache to initiative cell

--- decidim-initiatives/app/cells/decidim/initiatives/initiative_m_cell.rb
+++ decidim-initiatives/app/cells/decidim/initiatives/initiative_m_cell.rb
@@ -8,9 +8,23 @@ module Decidim
       include InitiativeHelper
       include Decidim::Initiatives::Engine.routes.url_helpers
 
+      cache :show do
+        Digest::MD5.hexdigest(cache_hash)
+      end
+
       property :state
 
       private
+
+      def cache_hash
+        hash = model.updated_at.to_s +
+            model.supports_count.to_s +
+            comments_count.to_s
+
+        hash << current_user.follows?(model).to_s if current_user
+
+        hash
+      end
 
       def translatable?
         true
