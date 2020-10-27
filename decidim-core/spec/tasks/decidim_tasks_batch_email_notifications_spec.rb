c494cc24011e459f1b14164f6e388175e307688a
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2020-10-27 11:59:48 +0100

    increase batch notifications tasks specs

--- decidim-core/spec/tasks/decidim_tasks_batch_email_notifications_spec.rb
+++ decidim-core/spec/tasks/decidim_tasks_batch_email_notifications_spec.rb
@@ -7,6 +7,10 @@ describe "rake decidim:batch_email_notifications:send", type: :task do
   let(:task_name) { :"decidim:batch_email_notifications:send" }
   let(:argument_error_output) { /Rake aborted !/ }
 
+  let!(:user) { create(:user) }
+  let!(:notifications) { create_list(:notification, 2, user: user) }
+  let!(:now_notifications) { create_list(:notification, 3, :now_priority, user: user) }
+
   context "when executing task" do
     it "raises an ArgumentError" do
       Rake::Task[task_name].reenable
@@ -25,6 +29,12 @@ describe "rake decidim:batch_email_notifications:send", type: :task do
       it "has to be executed without failures" do
         expect(Decidim::BatchEmailNotificationsGeneratorJob).to receive(:perform_later)
         Rake::Task[task_name].execute
+      end
+
+      it "send the batch notifications only" do
+        expect(Decidim::Notification.where(sent_at:nil).count).to eq(5)
+        Rake::Task[task_name].execute
+        expect(Decidim::Notification.where.not(sent_at:nil).count).to eq(3)
       end
 
       it "enqueues mailers" do
