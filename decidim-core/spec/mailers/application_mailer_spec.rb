84b6a8f91ee744420ecc44839a2a4e8860d436ca
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2020-11-20 16:19:13 +0100

    light cleanup application mailer specs

--- decidim-core/spec/mailers/application_mailer_spec.rb
+++ decidim-core/spec/mailers/application_mailer_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require "spec_helper"
-require "zip"
 
 module Decidim
   describe Decidim::DummyResources::DummyResourceMailer, type: :mailer do
@@ -19,28 +18,28 @@ module Decidim
           from: from
         }
       end
-      let(:mail) { described_class.fake_mail(user, organization) }
+      let(:mail) { described_class.send_email(user, organization) }
       let(:from) { "" }
 
       context "when there is no organization at all" do
-        let(:mail) { described_class.fake_mail(user, nil) }
+        let(:mail) { described_class.send_email(user, nil) }
 
-        it "returns values defined in Decidim.config" do
+        it "returns default values" do
           expect(mail.from).to eq(["change-me@example.org"])
           expect(mail.reply_to).to eq(nil)
         end
       end
 
-      context "when smtp_settings are not setted" do
+      context "when smtp_settings are not set" do
         let(:smtp_settings) { nil }
 
-        it "returns values defined in Decidim.config" do
+        it "returns default values" do
           expect(mail.from).to eq(["change-me@example.org"])
           expect(mail.reply_to).to eq(nil)
         end
       end
 
-      context "when from label is not setted" do
+      context "when from is not set" do
         let(:from) { nil }
 
         it "set default values for mail.from and mail.reply_to" do
@@ -49,7 +48,7 @@ module Decidim
         end
       end
 
-      context "when from label is setted" do
+      context "when from is set" do
         let(:from) { "Bruce Wayne <decide@gotham.org>" }
 
         it "set default values for mail.from and mail.reply_to" do
