7cb589840569ab0f290dd160b47b1f0c52f26138
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2021-02-22 18:03:02 +0100

    remove deprecated todo

--- decidim-initiatives/spec/permissions/decidim/initiatives/permissions_spec.rb
+++ decidim-initiatives/spec/permissions/decidim/initiatives/permissions_spec.rb
@@ -337,7 +337,6 @@ describe Decidim::Initiatives::Permissions do
       context "when user is not a member" do
         let(:initiative) { create :initiative, :discarded, organization: organization }
 
-        # TODO: Check again this test, maybe it should be equal to falsey
         it "restricts join committee request on creation" do
           expect(subject).to be_truthy
         end
