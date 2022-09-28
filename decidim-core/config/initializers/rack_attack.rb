a30080896f3e52862eb4b5938cebd7acdea8e091
Quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-09-28 16:13:47 +0200

    Fetch env key or return nil

--- decidim-core/config/initializers/rack_attack.rb
+++ decidim-core/config/initializers/rack_attack.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-if (ENV.fetch("ENABLE_RACK_ATTACK") == "1") || Rails.env.production? || Rails.env.test?
+if (ENV.fetch("ENABLE_RACK_ATTACK", nil) == "1") || Rails.env.production? || Rails.env.test?
   require "rack/attack"
 
   Rails.application.configure do |config|
@@ -13,6 +13,7 @@ if (ENV.fetch("ENABLE_RACK_ATTACK") == "1") || Rails.env.production? || Rails.en
       if request.path.start_with?("/system")
         next if Decidim.system_accesslist_ips.blank?
 
+        # returns true if request.ip is not included in system access list
         Decidim.system_accesslist_ips.select do |ip_address|
           IPAddr.new(ip_address).include?(IPAddr.new(request.ip))
         end.empty?
